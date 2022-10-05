4e8c084028ab4c017d37a9577949c769dc3266cd
Oddwarrior <94823030+Oddwarrior@users.noreply.github.com>
2022-10-05 22:36:11 +0530

    search fixed
    search is now working

--- main.js
+++ main.js
@@ -3,11 +3,21 @@ const API_KEY = "490d29383ee926971139befa321ac9ec";
 
 const DAYS_OF_THE_WEEK = ["sun", "mon", "tue" ,"wed","thu","fri","sat"];
 
+let selectedCityText;
+let selectedCity;
+
 //get fubctions --------------------------
 // current weather data
-const getCurrentWeatherData = async () => {
-    const city = "mumbai";
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
+
+const getCitiesUsingGeoLocation = async (searchText) =>{
+    const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${searchText}&limit=5&appid=${API_KEY}`);
+    return response.json()
+}
+
+//current weather data
+const getCurrentWeatherData = async ({lat, lon, name:city}) => {
+    const url = (lat&& lon)? `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`:`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+    const response = await fetch(url);
     return response.json()
 }
 //hourly weather data
@@ -84,6 +94,20 @@ const loadHumidity = ({main:{humidity}})=>{
     feelsLikeTemp.textContent = `${humidity} %`;
 }
 
+const loadData=async()=>{
+    const CurrentWeather = await getCurrentWeatherData(selectedCity);
+    console.log(CurrentWeather);
+    loadCurrentWeatherData(CurrentWeather);
+
+    const hourlyWeather = await getHourlyWeatherData(CurrentWeather);
+    loadHourlyWeatherData(CurrentWeather,hourlyWeather);
+
+    load5DayWeather(hourlyWeather);
+
+    loadFeelsLike(CurrentWeather);
+    loadHumidity(CurrentWeather);
+}
+
 
 //other functions--------------------
 const formatTemperature = (temp) => `${temp?.toFixed(1)}°`;
@@ -116,19 +140,69 @@ const calulateDayWiseWeather = (hourlyWeather) => {
     return dayWiseWeather;
 }
 
+//timer to wait for completion of typing
+function debounce(func){
+    let timer;
+    return (...args)=>{
+        clearTimeout(timer); // clear timeout
+        // create a new time user is typing
+        timer = setTimeout(() => {
+            console.log("debounce");
+            func.apply(this, args)
+        }, 500);
+    }
+}
+
+//after searching
+const onSearchChange =async (event)=>{
+    let {value} = event.target;
+    if(!value){
+        selectedCity=null;
+        selectedCityText=" ";
+    }
+
+    if(value && (selectedCityText!== value)){
+        const listOfCities = await getCitiesUsingGeoLocation(value);
+        let options ="";
+        for(let{ lat, lon, name, state, country}of listOfCities) {
+            options +=`<option  data-city-details='${JSON.stringify({lat, lon, name})}' value="${name},${state},${country}"></option>`
+        }
+    
+        document.querySelector("#cities").innerHTML = options;
+        console.log(listOfCities);
+    }
+
+}
+
+const handleCitySelection = (event)=>{
+    console.log("selection done");
+    selectedCityText = event.target.value;
+    let options= document.querySelectorAll("#cities > option")
+    console.log(options);
+    if(options?.length){
+        let selectedOption = Array.from(options).find(opt=>opt.value === selectedCityText);
+        selectedCity = JSON.parse(selectedOption.getAttribute("data-city-details"));
+        console.log({selectedCity});
+        loadData();
+    }
+
+}
+
+const loadForecastUsingGeoLoaction=()=>{
+navigator.geolocation.getCurrentPosition(({coords})=>{
+    const {latitude:lat , longitude:lon} = coords;
+    selectedCity = {lat,lon};
+    loadData();
+}, error=> console.log(error))
+}
+
+const debounceSearch = debounce((event)=> onSearchChange(event))
 
 //main function------------------
     document.addEventListener("DOMContentLoaded", async () => {
-        const CurrentWeather = await getCurrentWeatherData();
-        console.log(CurrentWeather);
-        loadCurrentWeatherData(CurrentWeather);
-
-        const hourlyWeather = await getHourlyWeatherData(CurrentWeather);
-        loadHourlyWeatherData(CurrentWeather,hourlyWeather);
-
-        load5DayWeather(hourlyWeather);
-
-        loadFeelsLike(CurrentWeather);
-        loadHumidity(CurrentWeather);
+        loadForecastUsingGeoLoaction()
+        const searchInput = document.querySelector("#search");
+        searchInput.addEventListener("input",debounceSearch)
+        searchInput.addEventListener("change",handleCitySelection)
 
     })
